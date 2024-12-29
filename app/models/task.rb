9313fd50e1877a8a3ec2356711621b41b5cc3471
gabrielmendes64 <gabriel10010100@gmail.com>
2024-12-29 10:07:39 -0300

    feat: integrate Task and TodoList models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :todo_list
+
+  validates :title, presence: true
+  validates :priority, inclusion: { in: %w[low medium high], message: "%{value} is not a valid priority" }
 end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,6 @@
 class TodoList < ApplicationRecord
+  has_many :tasks, dependent: :destroy
+
+  validates :title, presence: true
+  validates :priority, inclusion: { in: %w[low medium high], message: "%{value} is not a valid priority" }
 end
