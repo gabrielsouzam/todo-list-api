2f58b5d1db85e5c4d86e5b33d589f34bce951207
gabrielmendes64 <gabriel10010100@gmail.com>
2024-12-29 10:03:40 -0300

    feat: configure CORS and set default primary key to UUID

--- config/application.rb
+++ config/application.rb
@@ -8,20 +8,19 @@ Bundler.require(*Rails.groups)
 
 module TodoListApi
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.api_only = true
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
     config.autoload_lib(ignore: %w[assets tasks])
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "http://localhost:5173"
+        resource "*",
+                 headers: :any,
+                 methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+      end
+    end
   end
 end
