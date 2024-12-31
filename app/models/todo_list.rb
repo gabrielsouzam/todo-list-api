a9370de6930dd9a7a3d7ba83e5e9938e3dbebbdd
gabrielmendes64 <gabriel10010100@gmail.com>
2024-12-31 11:45:52 -0300

    feat: integrate todo_list table with user table

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,6 @@
 class TodoList < ApplicationRecord
   has_many :tasks, dependent: :destroy
+  belongs_to :user
 
   validates :title, presence: true
   validates :priority, inclusion: { in: %w[low medium high], message: "%{value} is not a valid priority" }
