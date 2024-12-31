44e301196b25de9d7d535d884175d1efc18ea7e4
gabrielmendes64 <gabriel10010100@gmail.com>
2024-12-31 11:46:19 -0300

    feat: add callbacks to update task count and completed tasks for todo_list

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,22 @@ class Task < ApplicationRecord
 
   validates :title, presence: true
   validates :priority, inclusion: { in: %w[low medium high], message: "%{value} is not a valid priority" }
+
+  after_create :increment_task_count
+  after_destroy :decrement_task_count
+  after_update :update_todo_list_tasks_done, if: :saved_change_to_done?
+
+  private
+
+  def increment_task_count
+    todo_list.increment!(:task_count)
+  end
+
+  def decrement_task_count
+    todo_list.decrement!(:task_count)
+  end
+
+  def update_todo_list_tasks_done
+    todo_list.update(tasks_done: todo_list.tasks.where(done: true).count)
+  end
 end
